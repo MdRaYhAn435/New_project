66425f2b1cf945cf491338e19be55bb305628d76
MdRaYhAn435 <mdrayhan143435@gmail.com>
2021-11-23 23:58:38 +0600

    first commit

---
+++ view/layout/js/custom.js
@@ -0,0 +1,35 @@
+function Datainsert(){
+  var cat_type_name = document.getElementById("cat_type_name").value;
+  var cat_type_code = document.getElementById("cat_type_code").value;
+  
+  // validation code
+  //alert(cat_type_name);
+
+  $.ajax({
+      method: "POST",
+      url:"index.php",
+      data: {
+          name:cat_type_name,
+          code:cat_type_code
+      },
+      success: function(data){
+          showData();
+      }
+      
+  });
+
+
+  
+  alert("successfully insarted");
+}
+function showData(){
+  $.ajax({
+      method: "POST",
+      url:"show.php",
+      success: function(data){
+          $('#show_data').html(data);
+          // document.getElementById("show_table_div").style.display="block";
+      }
+      
+  });  
+}
